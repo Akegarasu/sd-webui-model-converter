13542eb39ddc7d3cc802d13cf9c2997bbd1c0468
akiba <akiba@anzu.link>
2023-09-15 17:39:35 +0800

    fix bug, close #20

--- scripts/convert.py
+++ scripts/convert.py
@@ -122,7 +122,7 @@ def convert_warp(
             return do_convert(MockModelInfo(model_path), *args)
 
     elif model_name != "":
-        return do_convert(sd_models.checkpoints_list[model_name])
+        return do_convert(sd_models.checkpoints_list[model_name], *args)
 
     else:
         return "Error: must choose a model"
